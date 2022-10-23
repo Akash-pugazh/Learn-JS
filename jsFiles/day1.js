2344a001cd361ca3d426daa2f43ecb1d2ebe4043
Aakash Pugazh <89995392+Akash-pugazh@users.noreply.github.com>
2022-10-23 19:48:29 +0530

    Progressing DAY1 mid-commit

---
+++ jsFiles/day1.js
@@ -0,0 +1,28 @@
+"use strict"; //Enables the code to work in modern way
+// Must be enabled if it is only at top
+
+// alert("I'm JavaScript");
+
+// Linebreaks are implcit semicolons by Js
+
+// Leaving semicolon after a statement is a good practice
+
+// alert("New alert");
+
+// This is a single line comment
+
+/* 
+  This
+  is
+  a
+  multiline
+  comment
+*/
+
+
+// VARIABLES
+
+let message;
+message = "Hello , I'm a msg";
+
+console.log(message);
