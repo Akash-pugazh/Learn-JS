7c709e05e3b7f17bb95092f9dc6508102043d332
Aakash Pugazh <89995392+Akash-pugazh@users.noreply.github.com>
2022-10-23 20:32:08 +0530

    Variables✅

--- jsFiles/day1.js
+++ jsFiles/day1.js
@@ -19,10 +19,59 @@
   comment
 */
 
-
 // VARIABLES
 
-let message;
-message = "Hello , I'm a msg";
+let newMsg = "Hello , I'm a msg";
+newMsg = "I'm a modified msg";
+let myName = "Akash";
+let age = "19";
 
-console.log(message);
+console.log(newMsg, myName, age);
+
+var oldMsg = "I'm also a msg";
+console.log(oldMsg);
+
+let fullName = "Aakash";
+let myFullName = fullName;
+console.log("FullName :", fullName);
+console.log("MyFullName :", myFullName);
+
+// let $1stMsg = 1;
+// let $1stMsg = 2;
+// Declaring twice var cause error
+
+// s = 12; //without "use strict" variable can be used without declarations
+// It is a bad practice
+// console.log(s);
+
+const myBirthYear = 2003;
+// myBirthYear = 2002;
+// create assignment to a const variable error
+console.log(myBirthYear);
+
+// Uppercases are mostly used for const
+
+const MYBIRTHDATE = 13;
+const MYBIRTHMONTH = "July";
+console.log(
+  "I'm born in " + MYBIRTHDATE + " " + MYBIRTHMONTH + " " + myBirthYear
+);
+
+// TASKS
+
+let admin, $name;
+$name = "John";
+admin = $name;
+alert(admin);
+
+let planetName, currentUser;
+planetName = "Earth";
+currentUser = admin;
+
+const BIRTHDATE = "13.07.2003";
+const myAge = SomeCode(BIRTHDATE);
+
+// both are const but birthdate is fixed one cant changed 
+// but age changes with runtime of somecode func
+// so uppercase const applied only for birthdate not age
+
