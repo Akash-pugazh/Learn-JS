eab87f2e5e4d5c7232fbc6fea6ea1ad714f8669d
Aakash Pugazh <89995392+Akash-pugazh@users.noreply.github.com>
2022-10-24 15:34:19 +0530

    Interactions in JS ✅

--- DAY_2/day2.js
+++ DAY_2/day2.js
@@ -1,1 +1,21 @@
 "use strict";
+
+// * INTERACTION (ALERT, PROMPT, CONFIRM)
+
+// alert(`Click OK`);
+
+// let userName = prompt(`What is ur name ? `, "Boss");
+// alert("Welcome " + userName);
+
+// let isAdmin = confirm("Are you the Admin");
+// ? Returns true or false based on user selection
+// alert(isAdmin);
+
+// TASKS
+
+// let yourName = prompt(`What is your Name ? `, "Ghost");
+// alert(yourName);
+
+
+//  * TYPE CONVERSIONS
+
