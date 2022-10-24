7a17a6dd4944c0c6ce8e0383cf32231f2573785e
Aakash Pugazh <89995392+Akash-pugazh@users.noreply.github.com>
2022-10-24 15:47:37 +0530

    TypeConversion ✅

--- DAY_2/day2.js
+++ DAY_2/day2.js
@@ -19,3 +19,34 @@
 
 //  * TYPE CONVERSIONS
 
+let testingValue = true;
+console.log(typeof testingValue); //boolean
+// * String Conversion
+
+let testingNewValue = String(testingValue);
+console.log(typeof testingNewValue); //string
+
+// type conv in string numbers
+// ? +  considers them as string and uses for concatenation 
+// ? - * / converts them to numbers and do operation
+console.log("2" + "2");
+console.log("2" - "2");
+console.log("2" * "2");
+console.log("2" / "2");
+
+let str = "213"
+console.log(typeof str);
+
+// * Numeric conversion
+
+console.log(typeof Number(str));
+
+// * Boolean conversion
+
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(""));
+console.log(Boolean("Hello"));
+console.log(Boolean("0"));
+console.log(Boolean(" ")); //Spaces are true
+
