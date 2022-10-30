1c9ddbc9d7bdc4faf836ae03ee49ede55632ed80
Aakash Pugazh <89995392+Akash-pugazh@users.noreply.github.com>
2022-10-30 19:42:56 +0530

    Switch ✅

--- DAY_5/day5.js
+++ DAY_5/day5.js
@@ -1,1 +1,48 @@
 "use strict";
+
+//Switch
+
+let a = 2;
+switch (a) {
+  case 1:
+    console.log("little");
+    break;
+  case 2:
+    console.log("Exact");
+    break;
+  case 3:
+    console.log("Big");
+    break;
+  default:
+    console.log("Not matched");
+}
+// Breaks should be added
+
+// Tasks
+
+let browser = "Safari";
+if (browser === "Edge") {
+  console.log("You've got the Edge");
+} else if (
+  browser === "Chrome" ||
+  browser === "Firefox" ||
+  browser === "Safari" ||
+  browser === "Opera"
+) {
+  console.log("Okey we support these too");
+} else {
+  console.log("We hope this page looks ok");
+}
+
+let num = 1;
+switch(num){
+  case 0:
+    console.log(0);
+    break
+  case 1:
+    console.log(1);
+    break
+  case 2:
+  case 3:
+    console.log('2, 3');
+}
