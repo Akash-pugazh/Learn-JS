14979a48ec68d48a6a968cd774b151dae0f6738a
Aakash Pugazh <89995392+Akash-pugazh@users.noreply.github.com>
2022-10-31 10:27:25 +0530

    Started leaning objects

--- DAY_6/day6.js
+++ DAY_6/day6.js
@@ -17,3 +17,16 @@ function desname(desname) {
 // * Completed Code Quality
 // overview knowlege has beem gathered
 
+// ! OBJECTS
+
+// ? objects are used to store keyed collections of various data and more complex entities
+
+// created with {} and key : value pairs
+
+// empty object
+let userObj = new Object(); //obj constructor syntax
+let $userObj = {}; //obj literal syntax
+
+console.log(typeof userObj);
+console.log(typeof $userObj);
+
