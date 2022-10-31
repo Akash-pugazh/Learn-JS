4808b04ec6b7a48732b4b42ae47f39ac6b2634f5
Aakash Pugazh <89995392+Akash-pugazh@users.noreply.github.com>
2022-10-31 09:06:41 +0530

    .

---
+++ DAY_6/day6.js
@@ -0,0 +1,15 @@
+"use strict ";
+
+// ? CODE QUALITY
+
+// * DEBUGGING - process of finding and fixing errors within a script
+
+function hello(uName) {
+  desname(uName);
+  let desName = desname(uName);
+  console.log("Hello",desName);
+}
+
+function desname(desname) {
+  return `***${desname}***`;
+}
