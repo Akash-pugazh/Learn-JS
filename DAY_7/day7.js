4c38e2934a43a921d6b9a115d791e1c25452d73c
Aakash Pugazh <89995392+Akash-pugazh@users.noreply.github.com>
2022-11-04 23:08:25 +0530

    Garbages in obj

--- DAY_7/day7.js
+++ DAY_7/day7.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// * Garbage collection
+
+let user = {
+  name: "John",
+};
+console.log(user);
+let userCopy = user;
+userCopy.isUserCopy = true;
+user = null;
+
+console.log(user);
+console.log(userCopy);
+
+// ! This method in objects
